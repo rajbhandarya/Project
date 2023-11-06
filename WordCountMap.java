5493c97423da4e67a3cf0e42fd466cb84cfd5683
rajbhandarya <126826820+rajbhandarya@users.noreply.github.com>
2023-11-05 20:19:19 -0600

    Update WordCountMap.java

--- WordCountMap.java
+++ WordCountMap.java
@@ -4,14 +4,12 @@ public class WordCountMap {
      * tree that maintains a record of words and their counts. 
      * (WordCountMap will make use of two very small classes called Node and WordCount described below.)
      */
-    Node root;
+    private Node root;
     private class Node<E extends Comparable<E>>{
         private String word;
         private int count;
         private Node left;
         private Node right;
-        private E data;
-        Node root = null;
 
         public Node(String word, int count, Node left, Node right){
             this.word = word;
@@ -25,23 +23,37 @@ public class WordCountMap {
     public void insertWord(Node object){
         if (root == null){
             root = object;
+            return;
         }
-        else{
         Node current = root;
-        if (current.word.compareTo(object.word)>=0){
-            current.right = object;
-
-        }
         
-        if (current.word.compareTo(object.word)<0){
+        if (current.word.compareTo(object.word)>0){
+            while (current.left != null && current.word.compareTo(object.word)>0){
+            current = current.left;
+            }
+            if (current.word.compareTo(object.word)<0){
+                current.right = object;
+            }
             current.left = object;
         }
+        if (current.word.compareTo(object.word)<0){
+            while (current.right != null && current.word.compareTo(object.word)<0){
+            current = current.right;
+            }
+            current.right = object;
+        }
     }
-    }
+    //no child, add to left or right based on compareTo
+    //one child, add to left or right if it is null and in the right spot
+        //if there is a child in the way, and the child is less than, keep going
+        //if there is a child and the child is the wrong way, add it to the oppposite side of the child
+        
+
 
     public void insertCount(Node object){
         if (root == null){
             root = object;
+            return;
         }
         else{
 
@@ -57,19 +69,36 @@ public class WordCountMap {
     }
     }
 
+    public static void printInOrder(Node root){
+        if (root == null){
+            return;
+        }
+        Node current = root;
+        printInOrder(current.left);
+        System.out.println(current.word);
+        // System.out.println(current.right.word);
+        printInOrder(current.right);
+
+    }
+
     //maybe create getData function to get the data the root contains
 
     public static void main(String[] args){
         WordCountMap map = new WordCountMap();
-        Node node = map.new Node("alligator", 1, null, null);
-        System.out.println(node.root + "     " + node.word);
-        Node node2 = map.new Node("happy", 1, null, null);
+        Node root = map.new Node("apple", 2, null, null);
+        Node node = map.new Node("banana", 1, null, null);
+        Node node2 = map.new Node("aaron", 1, null, null);
+        Node node3 = map.new Node("dog", 1, null, null);
+        Node node4 = map.new Node("cat", 1, null, null);
+        Node node5 = map.new Node("zebra", 1, null, null);
+        map.insertWord(root);
+        map.insertWord(node);
         map.insertWord(node2);
-        System.out.println(map);
-        System.out.println(node.right);
-
+        map.insertWord(node3);
+        map.insertWord(node4);
+        map.insertWord(node5);
+        System.out.println(node3.right.word);
+        printInOrder(root);
 
     }
-
-    
 }
